925e8921d044f62c7cff6bee684374f9fecf0035
Rapala Tamás <rtamas19@gmail.com>
2019-05-17 13:58:10 +0200

    sortname

--- Project0517/src/project0517/Project0517.java
+++ Project0517/src/project0517/Project0517.java
@@ -5,6 +5,8 @@
  */
 package project0517;
 
+import java.util.List;
+
 /**
  *
  * @author Pepe
@@ -15,7 +17,13 @@ public class Project0517 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         
+        List<Person> people = Reader.read();
+
+        people.sort((Person a, Person b) -> a.getFullName().compareTo(b.getFullName()));
+        for (Person person : people) {
+            System.out.println(person.getFullName());
+        }
     }
     
 }
