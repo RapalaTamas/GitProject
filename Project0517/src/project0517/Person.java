584be1b09bbf1d745b676742568d8b042f3a8a29
Rapala Tamás <rtamas19@gmail.com>
2019-05-17 13:32:39 +0200

    add fields, getter..

---
+++ Project0517/src/project0517/Person.java
@@ -0,0 +1,34 @@
+package project0517;
+
+/**
+ *
+ * @author Pepe
+ */
+public class Person {
+    
+    String fullName;
+    String city;
+    String email;
+
+    public Person(String fullName, String city, String email) {
+        this.fullName = fullName;
+        this.city = city;
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+   
+    
+    
+}
